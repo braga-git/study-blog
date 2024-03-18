88f0df85591ccc7964fa8da8f1f5dccd03d3a045
Gabriel <gabrielcamara.contato@gmail.com>
2024-03-18 01:55:33 -0300

    feat: routes names function

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
-use App\Http\Controllers\AdminController;
-use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AdminController;
 
 
 Route::domain('admin.' . env('APP_URL'))->group(function () {
     Route::middleware('auth')->group(function () {
+
+        // Home
+        Route::get('/', [PagesController::class, 'home'])->name('pages.home');
+        // All posts
+        Route::get('/posts', [PagesController::class, 'posts'])->name('pages.posts');
 
         // Admin panel
         Route::get('/admin', [AdminController::class, 'index']);
@@ -20,29 +25,27 @@ Route::domain('admin.' . env('APP_URL'))->group(function () {
         // Store post
         Route::post('/posts', [PostController::class, 'store']);
         // Show edit post form
-        Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+        Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
         // Update post
         Route::put('/posts/{post}', [PostController::class, 'update']);
         // Delete post
-        Route::delete('/posts/{post}', [PostController::class, 'destroy']);
+        Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('post.destroy');
 
         Route::group(['middleware' => ['permission:view users|create users|edit users|delete users']], function () {
             // Manage users
             Route::get('/admin/users', [UserController::class, 'manage']);
-            // Show register user form
-            Route::get('/register', [UserController::class, 'create']);
+            // Show create user form
+            Route::get('/create', [UserController::class, 'create']);
             // Store user
             Route::post('/users', [UserController::class, 'store']);
             // Show edit user form
-            Route::get('/users/{user}/edit', [UserController::class, 'edit']);
+            Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('user.edit');
             // Update user
             Route::put('/users/{user}', [UserController::class, 'update']);
             // Delete user
-            Route::delete('/users/{user}', [UserController::class, 'destroy']);
+            Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('user.destroy');
         });
 
-        // All posts
-        Route::get('/', [PostController::class, 'index'])->name('posts.index');
         // Single post
         Route::get('/posts/{post}', [PostController::class, 'show']);
 
@@ -56,7 +59,9 @@ Route::domain('admin.' . env('APP_URL'))->group(function () {
     Route::post('/users/authenticate', [UserController::class, 'authenticate']);
 });
 
+// Home
+Route::get('/', [PagesController::class, 'home'])->name('pages.home');
 // All posts
-Route::get('/', [PostController::class, 'index']);
+Route::get('/posts', [PagesController::class, 'posts'])->name('pages.posts');
 // Single post
 Route::get('/posts/{post}', [PostController::class, 'show']);
