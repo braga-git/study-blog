8a692fd5bd4749d86c7313c7e4f8d7a4f146a85b
Gabriel <gabrielcamara.contato@gmail.com>
2024-03-18 01:51:18 -0300

    feat: show 3 latest most important posts at single post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,21 +1,21 @@
 <x-layout>
-    @include('partials._search')
+    <x-search :action="'/'" />
 
     <div>
         @auth
-        <div class="mb-6">
-            <div class="flex justify-end gap-6 mb-2">
-                <button class="text-gray-700">
-                    <a href="/posts/{{$post->id}}/edit"><i class="fa-solid fa-pen-to-square"></i> Edit</a>
-                </button>
-                <form method="POST" autocomplete="off" action="/posts/{{$post->id}}" class="text-red-500">
-                    @csrf
-                    @method('DELETE')
-                    <button><i class="fa-solid fa-trash"></i> Delete</button>
-                </form>
+            <div class="mb-6">
+                <div class="flex justify-end gap-6 mb-2">
+                    <button class="text-gray-700">
+                        <a href="/posts/{{ $post->id }}/edit"><i class="fa-solid fa-pen-to-square"></i> Edit</a>
+                    </button>
+                    <form method="POST" autocomplete="off" action="/posts/{{ $post->id }}" class="text-red-500">
+                        @csrf
+                        @method('DELETE')
+                        <button><i class="fa-solid fa-trash"></i> Delete</button>
+                    </form>
+                </div>
+                <hr>
             </div>
-            <hr>
-        </div>
         @endauth
         <h1 class="text-3xl font-semibold mb-4">
             {{ $post['title'] }}
@@ -29,7 +29,7 @@
 
         <div class="border rounded overflow-hidden my-8">
             <img class="w-full h-96 object-cover"
-                src="{{ $post->banner ? asset('storage/' . $post->banner) : asset('images/default_image.jpg') }}"
+                src="{{ $post->banner ? asset('storage/' . $post->banner) : asset('images/default_image.png') }}"
                 alt="">
         </div>
 
@@ -38,5 +38,21 @@
         </p>
 
         <x-post-tags :tagsCsv="$post->tags" />
+
+        <hr class="my-16">
+
+        <h1 class="text-3xl font-semibold mb-8">
+            Take a look at this too!
+        </h1>
+
+        <div class="grid sm:grid-cols-3 grid-cols-1 gap-4 space-y-0">
+            @if (count($posts) == 0)
+                <p>No posts found!</p>
+            @endif
+
+            @foreach ($posts as $post)
+                <x-post-card :post="$post" />
+            @endforeach
+        </div>
     </div>
 </x-layout>
